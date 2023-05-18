3ff6431d73cbbbcc731676169e2291cf054b4cf7
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-05-18 13:40:39 +0200

    feat: exchange access_code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useStore } from '@/stores/store'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      beforeEnter: async (to, from, next) => {
+        const store = useStore()
+        if (to.query.access_code) {
+          const accessCode = to.query.access_code as string
+          await store.exchangeAccessCode(accessCode)
+        }
+        next()
+      }
     }
   ]
 })
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,5 +1,9 @@
 import { defineStore } from 'pinia'
 
+const SPECKLE_CHALLENGE_KEY = 'SpeckleDemoApp.Challenge'
+const SPECKLE_AUTH_TOKEN_KEY = 'SpeckleDemoApp.AuthToken'
+const SPECKLE_AUTH_REFRESH_TOKEN_KEY = 'SpeckleDemoApp.AuthRefreshToken'
+
 interface State {
   user: any | null
 }
@@ -20,12 +24,34 @@ export const useStore = defineStore('store', {
         Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 
       // Store the challenge in localStorage
-      localStorage.setItem('SpeckleDemoApp.Challenge', challenge)
+      localStorage.setItem(SPECKLE_CHALLENGE_KEY, challenge)
 
       // Redirect to the Speckle Server auth page
       window.location.href = `${import.meta.env.VITE_SPECKLE_SERVER}/authn/verify/${
         import.meta.env.VITE_SPECKLE_APP_ID
       }/${challenge}`
+    },
+    async exchangeAccessCode(accessCode: string) {
+      const res = await fetch(`${import.meta.env.VITE_SPECKLE_SERVER}/auth/token/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          accessCode: accessCode,
+          appId: import.meta.env.VITE_SPECKLE_APP_ID,
+          appSecret: import.meta.env.VITE_SPECKLE_APP_SECRET,
+          challenge: localStorage.getItem(SPECKLE_CHALLENGE_KEY)
+        })
+      })
+      const data = await res.json()
+      if (data.token) {
+        // If retrieving the token was successful, remove challenge and set the new token and refresh token
+        localStorage.removeItem(SPECKLE_CHALLENGE_KEY)
+        localStorage.setItem(SPECKLE_AUTH_TOKEN_KEY, data.token)
+        localStorage.setItem(SPECKLE_AUTH_REFRESH_TOKEN_KEY, data.refreshToken)
+      }
+      return data
     }
   }
 })
