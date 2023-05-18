a62884f28b9b0f9778678480810d19fcfd200686
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2023-05-18 12:36:55 +0200

    feat: redirect to speckle login

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -12,5 +12,20 @@ export const useStore = defineStore('store', {
     isAuthenticated(): boolean {
       return this.user !== null
     }
+  },
+  actions: {
+    redirectToSpeckleAuthPage() {
+      // Generate a random challenge
+      const challenge =
+        Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+
+      // Store the challenge in localStorage
+      localStorage.setItem('SpeckleDemoApp.Challenge', challenge)
+
+      // Redirect to the Speckle Server auth page
+      window.location.href = `${import.meta.env.VITE_SPECKLE_SERVER}/authn/verify/${
+        import.meta.env.VITE_SPECKLE_APP_ID
+      }/${challenge}`
+    }
   }
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStore } from '@/stores/store'
+
+const store = useStore()
+
+if (!store.isAuthenticated) {
+  store.redirectToSpeckleAuthPage()
+}
+</script>
 
 <template>
   <main>My Speckle App</main>
